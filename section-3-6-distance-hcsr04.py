9d13edc00311f7c870716c57901ed47a6ae972fb
Gopal Amlekar <gopal.amlekar@gmail.com>
2017-09-12 13:58:26 +0530

    Rename files, add comments, tidy up

--- section-3-6-distance-hcsr04.py
+++ section-3-6-distance-hcsr04.py
@@ -11,22 +11,33 @@ print "Distance Measurement In Progress"
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
+# Stop transmission already going on, if any
 GPIO.output(TRIG, False)
 print "Waiting For Sensor To Settle"
 time.sleep(2)
 
+# 10 usec high-going pulse on trigger pin
 GPIO.output(TRIG, True)
 time.sleep(0.00001)
 GPIO.output(TRIG, False)
 
+# Wait till echo pin goes high
 while GPIO.input(ECHO)==0:
     pulse_start = time.time()
 
+# Now wait for the high period to be over i.e. till echo pin goes low
 while GPIO.input(ECHO)==1:
     pulse_end = time.time()
 
+# Pulse width in seconds
 pulse_duration = pulse_end - pulse_start
 
+# Consider velocity 343m/sec
+# Distance = velocity * time
+# Time measured here is two way - Transmit and recieve
+# So Distance = velocity * (Time /2)
+# With the velocity in cm, the formula becomes
+# Distance = 34300 * (time / 2) = 17150 * time
 distance = pulse_duration * 17150
 
 distance = round(distance, 2)
--- section-3-7-servo_motor.py
+++ section-3-7-servo_motor.py
@@ -3,23 +3,35 @@ import time
 
 servoPin = 23
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(servoPin, GPIO.OUT)
 
+# Configure PWM to generate 50Hz (A pulse every 20msec) frequency
+# Returns an object. Call various methods on the object
 servoPWM = GPIO.PWM(servoPin, 50)
 
+
+# Duty cycle = (ON time / Total time) * 100
+# For 0 degrees: (0.5 / 20) * 100 = 2.5
+# For 90 degrees:(1.5 / 20) * 100 = 7.5
+# For 180 degrees:(2.5 / 20) * 100 = 12.5
+# For any other angle, calculate accordingly using the formula above
+
+# Start PWM with duty cycle 7.5% (Initial default 90 degrees)
 servoPWM.start(7.5)
 
 try:
+    # Keep moving in 180 degree span from 0 to 90 to 180
     while True:
         servoPWM.ChangeDutyCycle(7.5)  # turn towards 90 degree
         time.sleep(1) # sleep 1 second
         servoPWM.ChangeDutyCycle(2.5)  # turn towards 0 degree
         time.sleep(1) # sleep 1 second
         servoPWM.ChangeDutyCycle(12.5) # turn towards 180 degree
-        time.sleep(1) # sleep 1 second 
-        
+        time.sleep(1) # sleep 1 second
+
 except KeyboardInterrupt:
+    # Must stop the PWM before exiting
 	servoPWM.stop()
 	GPIO.cleanup()
